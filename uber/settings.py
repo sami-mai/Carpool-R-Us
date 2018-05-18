92999d654f29c0dad6b722372c834d96676abb17
sami-mai <samirah.maison@gmail>
2018-05-18 18:08:56 +0300

    pilot deployment

--- uber/settings.py
+++ uber/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
           'PASSWORD': 'SmaiDB',
           }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
