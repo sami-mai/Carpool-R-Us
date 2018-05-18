75f9dc0f0e95e272b3c0e019ec2b130cefde4549
sami-mai <samirah.maison@gmail>
2018-05-18 15:47:59 +0300

    fix error with landing page

--- uber/settings.py
+++ uber/settings.py
@@ -144,5 +144,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login redirect
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/'
--- uber/urls.py
+++ uber/urls.py
@@ -13,18 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url  # , include
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views as project_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', project_views.landing, name='landing'),
-    # url(r'.rider', include('rider.urls')),
-    # url(r'.driver', include('driver.urls')),
-
+    url(r'^home$', project_views.landing, name='landing'),
+    url(r'^driver/', include('driver.urls')),
     # url(r'^rider/', include('rider.urls')),
-    # url(r'^driver/', include('driver.urls')),
+
+
+
+
+
+
 
 
 
