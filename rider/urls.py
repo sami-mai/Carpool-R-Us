7e6d8017c1960a169b147622ea72200f40e43ab3
sami-mai <samirah.maison@gmail>
2018-05-19 15:04:13 +0300

    add auth urls

--- rider/urls.py
+++ rider/urls.py
@@ -1,18 +1,15 @@
-from django.conf.urls import url
-from . import views as rider_views
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+# from . import views as rider_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # url('^$', views.home, name='home'),
-    # url(r'^login/', views.login, name="login"),
-    # url(r'^accounts/profile/(\d+)', views.user_profile, name="user_profile"),
-    # url(r'^accounts/edit-profile/(\d+)', views.edit_profile, name='edit_profile'),
-    # url(r'^accounts/upload-photo/', views.upload_photo, name='upload_photo'),
-    # url(r'^post-comment/(/d+)', views.post_comment, name='post_comment'),
-    # url(r'^search/', views.search_results, name='search_results'),
-    # url(r'^follow/(\d+)', views.follow, name="follow"),
-    # url(r'^likes/(\d+)', views.like_image, name='like_image'),
+
+    url(r'^accounts/login/', auth_views.login, name='login'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/logout/$', auth_views.logout, {"next_page": '/home'}),
+
 ]
 
 
