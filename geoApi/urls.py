b7ca69cf5150c63439d3384d6c6da70182b2292d
sami-mai <samirah.maison@gmail>
2018-05-21 15:16:07 +0300

    update

--- geoApi/urls.py
+++ geoApi/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-# from . import views as geo_views
+from . import views as geo_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
-    # url(r'^map/', views.home, name='home'),
+    # url(r'^map/', geo_views.map, name='map'),
 
 ]
 
